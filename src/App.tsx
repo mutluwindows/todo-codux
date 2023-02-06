525a32dc0a1dd15b7a70c4e22bd516c9617ae6b2
MutluPython <mutluwindows@gmail.com>
2023-02-06 00:12:14 +0000

    main screen text change from codux

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
+                    Mutlu was here...{' '}
                 </a>
             </header>
         </div>
