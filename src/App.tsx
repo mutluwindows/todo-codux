fb6bbf9ab81cd6c7b6ebdff9f28ed76cfee50d65
MutluPython <mutluwindows@gmail.com>
2023-02-06 00:13:59 +0000

    all

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Mutlu was here...{' '}
+                    Mutlu was NOT here. ..{' '}
                 </a>
             </header>
         </div>
